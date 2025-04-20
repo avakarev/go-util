97461d5a805be5ae2990f732a5cba708315f9252
Andrii Vakarev <avakarev@gmail.com>
2025-04-21 00:53:29 +0200

    natsutil: add subject to errors

--- natsutil/conn.go
+++ natsutil/conn.go
@@ -19,7 +19,7 @@ type Conn struct {
 	subscriptions map[*nats.Subscription]struct{}
 }
 
-func (c *Conn) envSubj(subj string) string {
+func (c *Conn) enrichSubj(subj string) string {
 	if strings.HasPrefix(subj, envutil.EnvDev) ||
 		strings.HasPrefix(subj, envutil.EnvBeta) ||
 		strings.HasPrefix(subj, envutil.EnvProd) {
@@ -30,7 +30,11 @@ func (c *Conn) envSubj(subj string) string {
 
 // Publish sends byte slice to the given subject
 func (c *Conn) Publish(subj string, data []byte) error {
-	return c.conn.Publish(c.envSubj(subj), data)
+	subj = c.enrichSubj(subj)
+	if err := c.conn.Publish(subj, data); err != nil {
+		return fmt.Errorf("%w, subj=%q", err, subj)
+	}
+	return nil
 }
 
 // PublishJSON marshalls given pointer destination into JSON and sends to the given subject
@@ -39,14 +43,15 @@ func (c *Conn) PublishJSON(subj string, data any) error {
 	if err != nil {
 		return err
 	}
-	return c.conn.Publish(c.envSubj(subj), bytes)
+	return c.Publish(subj, bytes)
 }
 
 // Subscribe subscribes given handler to the given subject
 func (c *Conn) Subscribe(subj string, fn nats.MsgHandler) error {
-	sub, err := c.conn.Subscribe(c.envSubj(subj), fn)
+	subj = c.enrichSubj(subj)
+	sub, err := c.conn.Subscribe(subj, fn)
 	if err != nil {
-		return err
+		return fmt.Errorf("%w, subj=%q", err, subj)
 	}
 	log.Debug().Str("subject", sub.Subject).Msg("nats: subscribed")
 	c.subscriptions[sub] = struct{}{}
@@ -55,13 +60,23 @@ func (c *Conn) Subscribe(subj string, fn nats.MsgHandler) error {
 
 // QueueSubscribe subscribes given handler to the given subject
 func (c *Conn) QueueSubscribe(subj string, queue string, fn nats.MsgHandler) error {
-	sub, err := c.conn.QueueSubscribe(c.envSubj(subj), queue, fn)
+	subj = c.enrichSubj(subj)
+	sub, err := c.conn.QueueSubscribe(subj, queue, fn)
 	if err != nil {
-		return err
+		return fmt.Errorf("%w, subj=%q", err, subj)
 	}
 	log.Debug().Str("subject", sub.Subject).Str("queue", queue).Msg("nats: subscribed")
 	c.subscriptions[sub] = struct{}{}
 	return nil
+}
+
+func (c *Conn) req(subj string, data []byte, timeout time.Duration) (*nats.Msg, error) {
+	subj = c.enrichSubj(subj)
+	msg, err := c.conn.Request(subj, data, timeout)
+	if err != nil {
+		return nil, fmt.Errorf("%w, subj=%q", err, subj)
+	}
+	return msg, nil
 }
 
 // Request sends request and returns reply's message
@@ -71,9 +86,9 @@ func (c *Conn) Request(subj string, v any, timeout time.Duration) (*nats.Msg, er
 		if err != nil {
 			return nil, err
 		}
-		return c.conn.Request(c.envSubj(subj), dataBytes, timeout)
+		return c.req(subj, dataBytes, timeout)
 	}
-	return c.conn.Request(c.envSubj(subj), nil, timeout)
+	return c.req(subj, nil, timeout)
 }
 
 // RequestBytes sends request and returns reply's bytes
@@ -87,11 +102,11 @@ func (c *Conn) RequestBytes(subj string, v any, timeout time.Duration) ([]byte, 
 
 // RequestJSON sends requests and unmarshals reply's json bytes into given destination
 func (c *Conn) RequestJSON(subj string, v any, timeout time.Duration, destPtr any) error {
-	bytes, err := c.RequestBytes(subj, v, timeout)
+	respBytes, err := c.RequestBytes(subj, v, timeout)
 	if err != nil {
 		return err
 	}
-	return json.Unmarshal(bytes, destPtr)
+	return json.Unmarshal(respBytes, destPtr)
 }
 
 // Close unsubscribes consumers and closes connections
