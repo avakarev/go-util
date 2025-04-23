ea8a784e0706619e0fdcf3fba596ca7895381976
Andrii Vakarev <avakarev@gmail.com>
2025-04-23 23:46:28 +0200

    envutil: cleanup

--- envutil/envutil.go
+++ envutil/envutil.go
@@ -13,50 +13,59 @@ func Str(name string) string {
 	return os.Getenv(name)
 }
 
-// MustStr returns environment variable and error if it's not set
-func MustStr(name string) (string, error) {
+// ShouldStr returns environment variable and error if it's not set
+func ShouldStr(name string) (string, error) {
 	value, ok := os.LookupEnv(name)
 	if !ok {
-		return "", fmt.Errorf("environment variable %q is not set", name)
+		return "", fmt.Errorf("environment variable %q is required, but not set", name)
 	}
 	if value == "" {
-		return "", fmt.Errorf("environment variable %q is empty", name)
+		return "", fmt.Errorf("environment variable %q is required, but empty", name)
 	}
 	return value, nil
 }
 
-// StrSlice returns environment variable split by given separator
-func StrSlice(name string, sep string) []string {
-	s := Str(name)
-	if s == "" {
-		return nil
+// MustStr is like ShouldStr but panics in case of error
+func MustStr(name string) string {
+	value, err := ShouldStr(name)
+	if err != nil {
+		panic(err)
 	}
-	return strings.Split(s, sep)
+	return value
 }
 
-// MustStrSlice returns environment variable split by given separator and errors if it's not set
-func MustStrSlice(name string, sep string) ([]string, error) {
-	s, err := MustStr(name)
+// ShouldStrSlice returns environment variable split by given separator and error if it's not set
+func ShouldStrSlice(name string, sep string) ([]string, error) {
+	str, err := ShouldStr(name)
 	if err != nil {
 		return nil, err
 	}
-	return strings.Split(s, sep), nil
+	return strings.Split(str, sep), nil
 }
 
-// Int returns environment variable as int64 and conversion error if any
-func Int(name string) (int64, error) {
-	s := Str(name)
-	if s == "" {
+// MustStrSlice is like ShouldStrSlice but panics in case of error
+func MustStrSlice(name string, sep string) []string {
+	value, err := ShouldStrSlice(name, sep)
+	if err != nil {
+		panic(err)
+	}
+	return value
+}
+
+// ShouldInt returns environment variable as int64 and conversion error if any
+func ShouldInt(name string) (int64, error) {
+	str := Str(name)
+	if str == "" {
 		return 0, nil
 	}
-	return strconv.ParseInt(s, 10, 64)
+	return strconv.ParseInt(str, 10, 64)
 }
 
-// MustInt returns environment variable and error if it's not set or conversion error if any
-func MustInt(name string) (int64, error) {
-	s, err := MustStr(name)
+// MustInt is like ShouldInt but panics in case of error
+func MustInt(name string) int64 {
+	i, err := ShouldInt(name)
 	if err != nil {
-		return 0, err
+		panic(err)
 	}
-	return strconv.ParseInt(s, 10, 64)
+	return i
 }
