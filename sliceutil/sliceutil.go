912385abe6cb26221c680a0c1f18bc899a323efd
Andrii Vakarev <avakarev@gmail.com>
2026-03-06 20:26:49 +0100

    feat(sliceutil): delete deprecated .Contains(), use built-in slices.Contains() instead

--- sliceutil/sliceutil.go
+++ sliceutil/sliceutil.go
@@ -1,16 +1,7 @@
 // Package sliceutil implements slice helpers
 package sliceutil
 
-import "slices"
-
 import "math/rand"
-
-// Contains checks whenther given element is a member of given slice
-//
-// Deprecated: use `Contains` from builtin `slices` pkg instead (in go 1.21+)
-func Contains[T string | int | int64 | float64](slice []T, elem T) bool {
-	return slices.Contains(slice, elem)
-}
 
 // Shuffle returns shuffled copy of given slice
 func Shuffle[T any](slice []T) []T {
